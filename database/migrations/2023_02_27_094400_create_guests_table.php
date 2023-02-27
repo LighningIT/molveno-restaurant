f34e0269ee1e110cd321467c0d829e01a1ef5be6
Esma <esma_yilmaz7@hotmail.com>
2023-02-27 13:35:44 +0100

    ficked migration guest

--- database/migrations/2023_02_27_094400_create_guests_table.php
+++ database/migrations/2023_02_27_094400_create_guests_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('firstname');
             $table->string('lastname');
-            $table->intger('phone_number');
+            $table->integer('phone_number');
             $table->boolean('hotel_guest');
             $table->timestamps();
         });
