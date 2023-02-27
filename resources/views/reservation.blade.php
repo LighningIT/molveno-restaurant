ec4f5961da6bd30095a47c3ae5cd82dae78bdcbc
ari <82609830+exaricode@users.noreply.github.com>
2023-02-27 15:58:57 +0100

    tailwind

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservation</title>
-</head>
-<body>
-    
-    <header class="bg-black">
-        <div>
-            <h1>Reservation</h1>
-        </div>
-    <header>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <main class="grid grid-cols-2">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+<body>
+
+
         @foreach ($tables as $table)
-            <p>{{$table}}</p>
+            <p class="bg-red-500">{{$table}}</p>
         @endforeach
-    </main>
+
+
+
+
 
 </body>
 </html>
