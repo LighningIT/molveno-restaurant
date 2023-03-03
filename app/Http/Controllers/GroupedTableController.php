00ee0a27d1b24603ef9bb254d3960b32be1ca43d
ari <82609830+exaricode@users.noreply.github.com>
2023-03-03 10:11:32 +0100

    fix: grouped table view

--- app/Http/Controllers/GroupedTableController.php
+++ app/Http/Controllers/GroupedTableController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 use App\Models\GroupedTable;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class GroupedTableController extends Controller
 {
     public static function getAllTable()
     {
-        return view('reservations',['tables'=>GroupedTable::getAllTable()]);
+        return view('reservations',[
+            'tables' => GroupedTable::getAllTable(),
+            'reservations' => Reservation::getAllReservations()]);
     }
 }
