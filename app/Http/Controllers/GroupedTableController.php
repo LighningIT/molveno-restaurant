25e7fc6a51e0a707b72ce69cff15ec5ed9aa0cf5
ari <82609830+exaricode@users.noreply.github.com>
2023-03-03 09:51:46 +0100

    fix: typo

--- app/Http/Controllers/GroupedTableController.php
+++ app/Http/Controllers/GroupedTableController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class GroupedTableController extends Controller
 {
-    public static function getAllTable() 
+    public static function getAllTable()
     {
-        return view('reservation',['tables'=>GroupedTable::getAllTable()]);
+        return view('reservations',['tables'=>GroupedTable::getAllTable()]);
     }
 }
