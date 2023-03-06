8eb4119b8269683d3a0b256aebd05da7c87351d0
ari <82609830+exaricode@users.noreply.github.com>
2023-03-06 14:08:58 +0100

    new reservation modal

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -2,6 +2,7 @@
 
         @foreach ($tables as $table)
         <x-tablegroups
+            class="bg-red-50"
             :id="$table->id"
             :tableSectionId="$table->table_section_id"
             :combined="$table->combined"
