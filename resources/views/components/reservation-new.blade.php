cc4251851ef0d4545fdea8d3faf1d9678ec28d90
ari <82609830+exaricode@users.noreply.github.com>
2023-04-03 11:23:09 +0200

    fix: remove double checkbtn

--- resources/views/components/reservation-new.blade.php
+++ resources/views/components/reservation-new.blade.php
@@ -32,10 +32,6 @@
             </div>
 
             <div>
-                <button type="button" id="checkBtn" name="checkBtn">check</button>
-            </div>
-
-            <div>
                 <label for="hotel-guest" class="block text-center">Guest hotel</label>
                 <input type="text" id="hotel-guest" name="guest">
             </div>
