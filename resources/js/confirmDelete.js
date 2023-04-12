00826c69d4e0c22d02d194e88a9278c2d68e7b30
ari <82609830+exaricode@users.noreply.github.com>
2023-04-12 13:49:17 +0200

    fix

--- resources/js/confirmDelete.js
+++ resources/js/confirmDelete.js
@@ -1,15 +1,17 @@
 const deleteModal = document.getElementById('deleteModal');
-const deleteForm = document.getElementById('deleteReservation');
+const deleteForm = document.querySelectorAll('.deleteReservation');
 
 document.addEventListener('DOMContentLoaded', () => {
-    deleteForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        toggleHiddenModal(deleteModal.parentElement);
-        deleteModal.querySelectorAll('button')[1].addEventListener('click', () => {
-            deleteForm.submit();
-        });
-        deleteModal.querySelectorAll('button')[0].addEventListener('click', () => {
+    deleteForm.forEach((elem) => {
+        elem.addEventListener('submit', (e) => {
+            e.preventDefault();
             toggleHiddenModal(deleteModal.parentElement);
+            deleteModal.querySelectorAll('button')[1].addEventListener('click', () => {
+                elem.submit();
+            });
+            deleteModal.querySelectorAll('button')[0].addEventListener('click', () => {
+                toggleHiddenModal(deleteModal.parentElement);
+            });
         });
     });
 });
