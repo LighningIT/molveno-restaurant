eae8de4087528502bcc6b6339164282efa87705e
ari <82609830+exaricode@users.noreply.github.com>
2023-05-03 14:09:59 +0200

    remove comments

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -13,24 +13,7 @@ class MenuSeeder extends Seeder
      */
     public function run(): void
     {
-        // for($i = 0; $i < 14; $i++) {
-        //     Menu::factory()->create([
-        //         "category_id" => $i + 1
-        //     ]);
-        // }
-
-        //for($i = 0; $i < 4; $i++) {
-            Menu::factory()->create(['name' => 'Food']);
-            Menu::factory()->create(['name' => 'Beverage']);
-                /* [
-                "meal_type_id" => $i + 1
-            ]);
-        } */
-
-        // for($i = 0; $i < 4; $i++) {
-        //     Menu::factory()->create([
-        //         "menu_item_id" => $i + 1
-        //     ]);
-        // }
+        Menu::factory()->create(['name' => 'Food']);
+        Menu::factory()->create(['name' => 'Beverage']);
     }
 }
