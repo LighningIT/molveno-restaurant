d0ccb9bac85c19e953c3ebaed2ff098df56fa387
whytievanie <esma_yilmaz7@hotmail.com>
2023-05-17 13:41:11 +0200

    Change hover out button

--- resources/views/components/reset-all-chairs-button.blade.php
+++ resources/views/components/reset-all-chairs-button.blade.php
@@ -1,4 +1,4 @@
-<button {{$attributes->merge(["class" => "bg-slate-400 px-4 py-2 text-white rounded hover:bg-red-500
+<button {{$attributes->merge(["class" => "bg-slate-500 px-4 py-2 text-white rounded hover:bg-slate-400
     dark:text-white justify-start cursor-pointer"])}}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
